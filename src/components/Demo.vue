b8b885b61d344177c00747d23b14225941039d3c
incheol <ddori@incheolui-MacBook-Pro.local>
2019-12-08 00:46:29 +0900

    bug fix

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,18 +1,13 @@
 <template>
     <div>
-        
-        <!--
-        <dr-check-combobox  style="width:120px" ref="chkCombo2" :datas="datas" v-model="selectedValue2" title="체크콤보">                            
-        </dr-check-combobox>
-        -->
+
         <div style="display:inline-block;">
-            <dr-check-combobox  style="width:120px" ref="chkCombo1" :datas="datas" v-model="selectedValue1" title="체크콤보">                            
+            <dr-check-combobox  style="width:120px" ref="chkCombo1" :datas="datas" v-model="selectedValue" title="체크콤보">                            
             </dr-check-combobox>
             <button @click="onClickSelectedValue">change Selected</button>
         </div>
         <div>
-            <span>{{selectedText1}}</span>
-            <span>{{selectedText2}}</span> 
+            <span>{{selectedText}}</span>
         </div>
 
     </div>
@@ -27,16 +22,12 @@ export default {
     data(){
         return{
             datas:[],
-            selectedValue1:[],
-            selectedValue2:[],
+            selectedValue:[],
         }
     },
     computed:{
-        selectedText1(){
-            return this.selectedValue1.join(" , ");
-        },
-        selectedText2(){
-            return this.selectedValue2.join(" , ");
+        selectedText(){
+            return this.selectedValue.join(" , ");
         }
     },
     components:{
@@ -51,16 +42,14 @@ export default {
         ];
 
         this.$nextTick(()=>{
-            this.selectedValue1 = [1,3];
+            this.selectedValue = [1,3];
         })
     },
     methods:{
         onClickSelectedValue(){
-            this.selectedValue1 = [3,4];
+            this.selectedValue = [3,4];
         }
     }
-
-    
 }
 </script>
 
